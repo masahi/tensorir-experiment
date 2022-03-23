fb5d4f6156c1c7556e43fa52111112708d649c93
Masahiro Masuda <masahi129@gmail.com>
2022-03-23 18:34:05 +0900

    arm tensor intrin wip

---
+++ arm_common.py
@@ -0,0 +1,96 @@
+from tvm import tir
+from tvm.script import tir as T
+from tvm.script.registry import register
+
+
+@register
+def int32x4(imm, span):
+    return imm.astype("int32x4", span)
+
+
+@register
+def int8x4(imm, span):
+    return imm.astype("int8x4", span)
+
+
+@T.prim_func
+def dot_product_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, (4,), "int8", offset_factor=1)
+    B = T.match_buffer(b, (4, 4), "int8", offset_factor=1)
+    C = T.match_buffer(c, (4,), "int32", offset_factor=1)
+
+    with T.block("root"):
+        T.reads(C[0:4], A[0:4], B[0:4, 0:4])
+        T.writes(C[0:4])
+        for i in T.serial(0, 4):
+            with T.init():
+                C[i] = T.int32(0)
+            for k in T.serial(0, 4):
+                with T.block("update"):
+                    vi, vk = T.axis.remap("SR", [i, k])
+                    C[vi] = C[vi] + T.cast(A[vk], "int32") * T.cast(B[vi, vk], "int32")
+
+
+@T.prim_func
+def dot_int8_int8_int32_neon(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, (4,), "int8", offset_factor=1)
+    B = T.match_buffer(b, (4, 4), "int8", offset_factor=1)
+    C = T.match_buffer(c, (4,), "int32", offset_factor=1)
+
+    with T.block("root"):
+        T.reads(C[0:4], A[0:4], B[0:4, 0:4])
+        T.writes(C[0:4])
+
+        # # TODO: How to make it work
+        # A_int8 = A.vload([0], "int8x4")
+        # re_int32 = T.reinterpret(A_int8, dtype="int32")
+        # vec_ai32 = T.broadcast(re_int32, 4)
+
+        # vec_b = B.vload([0, 0], dtype="int8x8")
+
+        # multiply = T.call_llvm_pure_intrin(
+        #     T.llvm_lookup_intrinsic_id("llvm.aarch64.neon.smull.v8i16"),
+        #     T.uint32(0),
+        #     vec_ai32,
+        #     vec_b,
+        #     dtype="int32x4",
+        # )
+
+        # pair1 = T.call_llvm_pure_intrin(
+        #     T.llvm_lookup_intrinsic_id("llvm.aarch64.neon.saddlp.v4i32.v8i16"),
+        #     T.uint32(1),
+        #     multiply,
+        #     dtype="int32x4",
+        # )
+
+        # vec_b = B.vload([2, 0], dtype="int8x8")
+
+        # multiply = T.call_llvm_pure_intrin(
+        #     T.llvm_lookup_intrinsic_id("llvm.aarch64.neon.smull.v8i16"),
+        #     T.uint32(0),
+        #     vec_ai32,
+        #     vec_b,
+        #     dtype="int32x4",
+        # )
+
+        # pair2 = T.call_llvm_pure_intrin(
+        #     T.llvm_lookup_intrinsic_id("llvm.aarch64.neon.saddlp.v4i32.v8i16"),
+        #     T.uint32(1),
+        #     multiply,
+        #     dtype="int32x4",
+        # )
+
+        C[T.ramp(T.int32(0), 1, 4)] += T.call_llvm_pure_intrin(
+            T.llvm_lookup_intrinsic_id("llvm.aarch64.neon.addp.v4i32"),
+            T.uint32(2),
+            # pair1,
+            # pair2,
+            T.int32x4(0),
+            T.int32x4(0),
+            dtype="int32x4",
+        )
+
+
+tir.TensorIntrin.register(
+    "dot_int8_int8_int32_neon", dot_product_desc, dot_int8_int8_int32_neon
+)
