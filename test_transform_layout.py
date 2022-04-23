c286b0fecc0d1098d7c0800f56772667894ab37e
Masahiro Masuda <masahi129@gmail.com>
2022-04-23 11:08:12 +0900

    ldmatrix & mma tensorization working

--- test_transform_layout.py
+++ test_transform_layout.py
@@ -5,18 +5,162 @@ import tvm.testing
 from tvm import te
 from tvm.te import create_prim_func
 from tvm.tir import Schedule
+from tvm.script import tir as T
+from tvm import tir
 
+
+@T.prim_func
+def ldmatrix_a_desc(a: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(
+        a, (16, 16), "float16", align=128, offset_factor=16, scope="shared"
+    )
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+
+    with T.block("root"):
+        T.reads(A[0:16, 0:16])
+        T.writes(C[0:32, 0:8])
+
+        for ax0, ax1 in T.grid(16, 16):
+            with T.block("A_shared_warp"):
+                v0, v1 = T.axis.remap("SS", [ax0, ax1])
+                T.reads(A[v0, v1])
+                T.writes(
+                    C[v0 % 8 * 4 + v1 % 8 // 2, v1 // 8 * 4 + v0 // 8 * 2 + v1 % 2]
+                )
+                C[v0 % 8 * 4 + v1 % 8 // 2, v1 // 8 * 4 + v0 // 8 * 2 + v1 % 2] = A[
+                    v0, v1
+                ]
+
+
+@T.prim_func
+def ldmatrix_a_impl(a: T.handle, c: T.handle) -> None:
+    s1 = T.var("int32")
+    s0 = T.var("int32")
+    A = T.match_buffer(
+        a,
+        (16, 16),
+        "float16",
+        align=128,
+        offset_factor=16,
+        scope="shared",
+        strides=[s1, s0],
+    )
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+    tx = T.env_thread("threadIdx.x")
+
+    with T.block("root"):
+        T.reads(A[0:16, 0:16])
+        T.writes(C[0:32, 0:8])
+
+        T.evaluate(
+            T.ptx_ldmatrix(
+                0,
+                4,
+                ".b16",
+                C.data,
+                8 * tx,
+                A.data,
+                16 * (tx % 16) + 8 * (tx // 16),
+                dtype="float16",
+            )
+        )
+
+
+@T.prim_func
+def mma_sync_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+    B = T.match_buffer(b, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+    C = T.match_buffer(c, (32, 8), "float32", align=128, offset_factor=16, scope="warp")
+
+    with T.block("root"):
+        T.reads(C[0:32, 0:8], A[0:32, 0:8], B[0:32, 0:8])
+        T.writes(C[0:32, 0:8])
+        for i, j, k in T.grid(16, 16, 16):
+            with T.block("C"):
+                i, j, k = T.axis.remap("SSR", [i, j, k])
+                T.reads(
+                    C[i % 8 * 4 + j % 8 // 2, j // 8 * 4 + i // 8 * 2 + j % 2],
+                    A[i % 8 * 4 + k % 8 // 2, k // 8 * 4 + i // 8 * 2 + k % 2],
+                    B[j % 8 * 4 + k % 8 // 2, k // 8 * 4 + j // 8 * 2 + k % 2],
+                )
+                T.writes(C[i % 8 * 4 + j % 8 // 2, j // 8 * 4 + i // 8 * 2 + j % 2])
+                C[i % 8 * 4 + j % 8 // 2, j // 8 * 4 + i // 8 * 2 + j % 2] = C[
+                    i % 8 * 4 + j % 8 // 2, j // 8 * 4 + i // 8 * 2 + j % 2
+                ] + T.cast(
+                    A[i % 8 * 4 + k % 8 // 2, k // 8 * 4 + i // 8 * 2 + k % 2],
+                    "float32",
+                ) * T.cast(
+                    B[j % 8 * 4 + k % 8 // 2, k // 8 * 4 + j // 8 * 2 + k % 2],
+                    "float32",
+                )
+
+
+@T.prim_func
+def mma_sync_impl(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+    B = T.match_buffer(b, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
+    C = T.match_buffer(c, (32, 8), "float32", align=128, offset_factor=16, scope="warp")
+
+    with T.block("root"):
+        T.reads(C[0:32, 0:8], A[0:32, 0:8], B[0:32, 0:8])
+        T.writes(C[0:32, 0:8])
+        tx = T.env_thread("threadIdx.x")
+        T.evaluate(
+            T.ptx_mma(
+                "m16n8k16",
+                "row",
+                "col",
+                "fp16",
+                "fp16",
+                "fp32",
+                A.data,
+                A.elem_offset + tx * 8,
+                B.data,
+                B.elem_offset + tx * 8,
+                C.data,
+                C.elem_offset + tx * 8,
+                False,
+                dtype="float32",
+            )
+        )
+
+        T.evaluate(
+            T.ptx_mma(
+                "m16n8k16",
+                "row",
+                "col",
+                "fp16",
+                "fp16",
+                "fp32",
+                A.data,
+                A.elem_offset + tx * 8 + 4,
+                B.data,
+                B.elem_offset + tx * 8 + 4,
+                C.data,
+                C.elem_offset + tx * 8 + 4,
+                False,
+                dtype="float32",
+            )
+        )
+
+
+tir.TensorIntrin.register("mma.ldmatrix_a", ldmatrix_a_desc, ldmatrix_a_impl)
+tir.TensorIntrin.register("mma.mma_sync", mma_sync_desc, mma_sync_impl)
 
 def dense(n: int, m: int, k: int):
-    a = te.placeholder((n, k), name="A")
-    b = te.placeholder((m, k), name="B")
+    a = te.placeholder((n, k), name="A", dtype="float16")
+    b = te.placeholder((m, k), name="B", dtype="float16")
     k = te.reduce_axis((0, k), name="k")
     c = te.compute(
         (n, m),
-        lambda i, j: te.sum(a[i, k] * b[j, k], axis=[k]),
+        lambda i, j: te.sum(
+            tvm.tir.Cast("float32", a[i, k]) * tvm.tir.Cast("float32", b[j, k]),
+            axis=[k],
+        ),
         name="C",
     )
     return (a, b, c)
+
 
 matmul = create_prim_func(dense(n=16, m=16, k=16))
 
@@ -26,44 +170,43 @@ block = sch.get_block("C")
 A_shared = sch.cache_read(block, 0, "shared")
 B_shared = sch.cache_read(block, 1, "shared")
 
+
 def shared_16x16_to_ldmatrix_32x8_layout(i, j):
     thread_id = 4 * (i % 8) + (j % 8) // 2
     matrix_id = 2 * (j // 8) + (i // 8)
 
     return thread_id, 2 * matrix_id + j % 2
 
+
 block = sch.get_block("C")
 A_warp = sch.cache_read(block, 0, "warp")
 B_warp = sch.cache_read(block, 1, "warp")
+C_shared = sch.cache_write(block, 0, "shared")
+C_warp = sch.cache_write(block, 0, "warp")
 
-sch.transform_layout(
-    A_warp,
-    0,
-    "write",
-    index_map=shared_16x16_to_ldmatrix_32x8_layout
-)
+sch.transform_layout(A_warp, 0, "write", index_map=shared_16x16_to_ldmatrix_32x8_layout)
+sch.transform_layout(B_warp, 0, "write", index_map=shared_16x16_to_ldmatrix_32x8_layout)
+sch.transform_layout(C_warp, 0, "read", index_map=shared_16x16_to_ldmatrix_32x8_layout)
 
-sch.transform_layout(
-    B_warp,
-    0,
-    "write",
-    index_map=shared_16x16_to_ldmatrix_32x8_layout
-)
+block_init_c = sch.decompose_reduction(block, sch.get_loops(block)[0])
+sch.tensorize(sch.get_loops(A_warp)[0], "mma.ldmatrix_a")
+sch.tensorize(sch.get_loops(B_warp)[0], "mma.ldmatrix_a")
+sch.tensorize(sch.get_loops(block)[0], "mma.mma_sync")
 
 print(sch.mod.script())
 
 # print(tvm.lower(sch.mod["main"]))
-f = tvm.build(sch.mod["main"], target="llvm", name="dense")
-dev = tvm.cpu(0)
+# f = tvm.build(sch.mod["main"], target="llvm", name="dense")
+# dev = tvm.cpu(0)
 
-a_np = np.random.uniform(size=(16, 16)).astype("float32")
-b_np = np.random.uniform(size=(16, 16)).astype("float32")
-c_np = np.dot(a_np, b_np.transpose())
+# a_np = np.random.uniform(size=(16, 16)).astype("float16")
+# b_np = np.random.uniform(size=(16, 16)).astype("float16")
+# c_np = np.dot(a_np.astype("float32"), b_np.transpose().astype("float32"))
 
-a = tvm.nd.array(a_np, dev)
-b = tvm.nd.array(b_np, dev)
-c = tvm.nd.array(np.zeros((16, 16), dtype="float32"), dev)
+# a = tvm.nd.array(a_np, dev)
+# b = tvm.nd.array(b_np, dev)
+# c = tvm.nd.array(np.zeros((16, 16), dtype="float32"), dev)
 
-# print(f.imported_modules[0].get_source())
-f(a, b, c)
-tvm.testing.assert_allclose(c.numpy(), c_np, rtol=1e-3)
+# # print(f.imported_modules[0].get_source())
+# f(a, b, c)
+# tvm.testing.assert_allclose(c.numpy(), c_np, rtol=1e-3)
--- wuwei/tir_tensor_intrin_mma.py
+++ wuwei/tir_tensor_intrin_mma.py
@@ -7,18 +7,21 @@ from tvm import tir
 
 @T.prim_func
 def ldmatrix_a_desc(a: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16,
+    A = T.match_buffer(a, (16, 16, "float16", align=128, offset_factor=16,
                          scope="shared")
-    C = T.match_buffer(c, (16,16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_a")
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16,
+                         scope="warp")
 
     with T.block("root"):
         T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j in T.grid(16, 16):
-            with T.block("load"):
-                vii, vjj = T.axis.remap("SS", [i, j])
-                C[vii, vjj] = A[vii, vjj]
+        T.writes(C[0 : 32, 0 : 8])
+
+        for ax0, ax1 in T.grid(16, 16):
+            with T.block("A_shared_warp"):
+                v0, v1 = T.axis.remap("SS", [ax0, ax1])
+                T.reads(A[v0, v1])
+                T.writes(C[v0 % 8 * 4 + v1 % 8 // 2, v1 // 8 * 4 + v0 // 8 * 2 + v1 % 2])
+                C[v0 % 8 * 4 + v1 % 8 // 2, v1 // 8 * 4 + v0 // 8 * 2 + v1 % 2] = A[v0, v1]
 
 
 @T.prim_func
@@ -26,12 +29,12 @@ def ldmatrix_a_impl(a: T.handle, c: T.handle) -> None:
     s1 = T.var("int32")
     s0 = T.var("int32")
     A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16, scope="shared", strides=[s1, s0])
-    C = T.match_buffer(c, (8,), "float16", align=128, offset_factor=16, scope="local")
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
     tx = T.env_thread("threadIdx.x")
 
     with T.block("root"):
         T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 8])
+        T.writes(C[0 : 32, 0 : 8])
 
         T.evaluate(
             T.ptx_ldmatrix(
@@ -39,8 +42,8 @@ def ldmatrix_a_impl(a: T.handle, c: T.handle) -> None:
                 4,
                 ".b16",
                 C.data,
-                0,
-                C.data,
+                8 * tx,
+                A.data,
                 16 * (tx % 16) + 8 * (tx // 16),
                 dtype="float16",
             )
@@ -48,16 +51,16 @@ def ldmatrix_a_impl(a: T.handle, c: T.handle) -> None:
 
 
 @T.prim_func
-def ldmatrx_b_desc(a: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16,
+def ldmatrix_b_desc(a: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16,
                          scope="shared")
-    C = T.match_buffer(c, (16, 16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_b")
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16,
+                         scope="warp")
 
     with T.block("root"):
-        T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j in T.grid(16, 16):
+        T.reads(A[0 : 32, 0 : 8])
+        T.writes(C[0 : 32, 0 : 8])
+        for i, j in T.grid(32, 8):
             with T.block("load"):
                 vii, vjj = T.axis.remap("SS", [i, j])
                 C[vii, vjj] = A[vii, vjj]
@@ -67,121 +70,101 @@ def ldmatrx_b_desc(a: T.handle, c: T.handle) -> None:
 def ldmatrix_b_impl(a: T.handle, c: T.handle) -> None:
     s1 = T.var("int32")
     s0 = T.var("int32")
-    A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16, scope="shared", strides=[s1, s0])
-    C = T.match_buffer(c, (8,), "float16", align=128, offset_factor=16, scope="local")
-
+    A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16, scope="shared", strides=[s1, s0])
+    C = T.match_buffer(c, (32, 8), "float16", align=128, offset_factor=16, scope="warp")
     tx = T.env_thread("threadIdx.x")
 
-
     with T.block("root"):
-        T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 8])
+        T.reads(A[0 : 32, 0 : 8])
+        T.writes(C[0 : 32, 0 : 8])
 
         T.evaluate(
             T.ptx_ldmatrix(
                 0,
                 4,
                 ".b16",
+                A.data,
+                8 * tx,
                 C.data,
-                0,
-                C.data,
-                16 * (tx % 16) + 8 * (tx // 16),
+                8 * tx,
                 dtype="float16",
             )
         )
 
-@T.prim_func
-def mma_sync_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_a")
-    B = T.match_buffer(b, (16, 16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_b")
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16,
-                         scope="wmma.accumulator")
 
-    with T.block("root"):
-        T.reads(C[0 : 16, 0 : 16], A[0 : 16, 0 : 16], B[0: 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j, k in T.grid(16, 16, 16):
-            with T.block(""):
-                vii, vjj, vkk = T.axis.remap("SSR", [i, j, k])
-                C[vii, vjj] = C[vii, vjj] + T.cast(A[vii, vkk], 'float32') * T.cast(B[vkk, vjj], 'float32')
+# @T.prim_func
+# def mma_sync_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
+#     A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16,
+#                          scope="warp")
+#     B = T.match_buffer(b, (32, 8), "float16", align=128, offset_factor=16,
+#                          scope="warp")
+#     C = T.match_buffer(c, (32, 8), "float32", align=128, offset_factor=16,
+#                          scope="warp")
 
-
-@T.prim_func
-def mma_sync_impl(a: T.handle, b: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_a")
-    B = T.match_buffer(b, (16, 16), "float16", align=128, offset_factor=16,
-                         scope="wmma.matrix_b")
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16,
-                         scope="wmma.accumulator")
-
-    with T.block("root"):
-        T.reads(C[0 : 16, 0 : 16], A[0 : 16, 0 : 16], B[0: 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        T.evaluate(T.tvm_mma_sync(C.data, C.elem_offset // 256 + T.floordiv(T.floormod(C.elem_offset, 256), 16),
-                                  A.data, A.elem_offset // 256 + T.floordiv(T.floormod(A.elem_offset, 256), 16),
-                                  B.data, B.elem_offset // 256 + T.floordiv(T.floormod(B.elem_offset, 256), 16),
-                                  C.data, C.elem_offset // 256 + T.floordiv(T.floormod(C.elem_offset, 256), 16), dtype='handle'))
+#     with T.block("root"):
+#         T.reads(C[0 : 32, 0 : 8], A[0 : 32, 0 : 8], B[0: 32, 0 : 8])
+#         T.writes(C[0 : 32, 0 : 8])
+#         for i, j, k in T.grid(16, 16, 16):
+#             with T.block("C"):
+#                 i, j, k = T.axis.remap("SSR", [i0, i1, i2])
+#                 T.reads(A_shared_warp[i % 8 * 4 + k % 8 // 2, k // 8 * 4 + i // 8 * 2 + k % 2], B_shared_warp[j % 8 * 4 + k % 8 // 2, k // 8 * 4 + j // 8 * 2 + k % 2])
+#                 T.writes(C[i, j])
+#                 C[i, j] = C[i, j] + A_shared_warp[i % 8 * 4 + k % 8 // 2, k // 8 * 4 + i // 8 * 2 + k % 2] * B_shared_warp[j % 8 * 4 + k % 8 // 2, k // 8 * 4 + j // 8 * 2 + k % 2]
 
 
-@T.prim_func
-def mma_fill_desc(c: T.handle) -> None:
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16, scope="wmma.accumulator")
 
-    with T.block("root"):
-        T.reads()
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j in T.grid(16, 16):
-            with T.block("init"):
-                vii, vjj = T.axis.remap("SS", [i, j])
-                C[vii, vjj] = T.float32(0)
+# @T.prim_func
+# def mma_sync_impl(a: T.handle, b: T.handle, c: T.handle) -> None:
+#     A = T.match_buffer(a, (32, 8), "float16", align=128, offset_factor=16,
+#                          scope="warp")
+#     B = T.match_buffer(b, (32, 8), "float16", align=128, offset_factor=16,
+#                          scope="warp")
+#     C = T.match_buffer(c, (32, 8), "float32", align=128, offset_factor=16,
+#                          scope="warp")
 
+#     with T.block("root"):
+#         T.reads(C[0 : 32, 0 : 8], A[0 : 32, 0 : 8], B[0: 32, 0 : 8])
+#         T.writes(C[0 : 32, 0 : 8])
+#         tx = T.env_thread("threadIdx.x")
+#         T.evaluate(
+#             T.ptx_mma(
+#                 "m16n8k16",
+#                 "row",
+#                 "col",
+#                 "fp16",
+#                 "fp16",
+#                 "fp16",
+#                 A.data,
+#                 A.elem_offset + tx * 8,
+#                 B.data,
+#                 B.elem_offset + tx * 8,
+#                 C.data,
+#                 C.elem_offset + tx * 8,
+#                 False,
+#                 dtype="float16",
+#             )
+#         )
 
-@T.prim_func
-def mma_fill_impl(c: T.handle) -> None:
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16, scope="wmma.accumulator")
-    with T.block("root"):
-        T.reads()
-        T.writes(C[0 : 16, 0 : 16])
-        T.evaluate(T.tvm_fill_fragment(C.data, 16, 16, 16, C.elem_offset // 256 + T.floordiv(T.floormod(C.elem_offset, 256), 16), T.float32(0), dtype="handle"))
-
-
-@T.prim_func
-def mma_store_desc(a: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), "float32", align=128, offset_factor=16, scope="wmma.accumulator")
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16, scope="global")
-    with T.block("root"):
-        T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j in T.grid(16, 16):
-            with T.block("store"):
-                vii, vjj = T.axis.remap("SS", [i, j])
-                C[vii, vjj] = A[vii, vjj]
-
-
-@T.prim_func
-def mma_store_impl(a: T.handle, c: T.handle) -> None:
-    s1 = T.var("int32")
-    s0 = T.var("int32")
-    A = T.match_buffer(a, (16, 16), "float32", align=128, offset_factor=16, scope="wmma.accumulator")
-    C = T.match_buffer(c, (16, 16), "float32", align=128, offset_factor=16, scope="global", strides=[s1, s0])
-    with T.block("root"):
-        T.reads(A[0 : 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        T.evaluate(T.tvm_store_matrix_sync(
-            A.data, 16, 16, 16, A.elem_offset // 256 + T.floordiv(T.floormod(A.elem_offset, 256), 16), C.access_ptr("w"), s1, "row_major",
-            dtype="handle"))
-
+#         T.evaluate(
+#             T.ptx_mma(
+#                 "m16n8k16",
+#                 "row",
+#                 "col",
+#                 "fp16",
+#                 "fp16",
+#                 "fp16",
+#                 A.data,
+#                 A.elem_offset + tx * 8 + 4,
+#                 B.data,
+#                 B.elem_offset + tx * 8 + 4,
+#                 C.data,
+#                 C.elem_offset + tx * 8 + 4,
+#                 False,
+#                 dtype="float16",
+#             )
+#         )
 
 
 tir.TensorIntrin.register("mma.ldmatrix_a", ldmatrix_a_desc, ldmatrix_a_impl)
 tir.TensorIntrin.register("mma.ldmatrix_b", ldmatrix_b_desc, ldmatrix_b_impl)
-
 tir.TensorIntrin.register("mma.mma_sync", mma_sync_desc, mma_sync_impl)
-tir.TensorIntrin.register("mma.fill", mma_fill_desc, mma_fill_impl)
-tir.TensorIntrin.register("mma.store", mma_store_desc, mma_store_impl)
-
-
-# ---------------------
