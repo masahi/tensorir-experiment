386ab4e5c073d4383e738a7b5cc6b2b4af498d93
Masahiro Masuda <masahi129@gmail.com>
2022-06-04 06:31:56 +0900

    shared store from row major B looks ok

--- permute_test.py
+++ permute_test.py
@@ -80,29 +80,21 @@ def ldmatrix_permuted_b_trans_layout():
 
     for lane_id in range(32):
         for row_pair_id in range(2):
-            row_id = row_pair_id * 2 + (lane_id // 16)
             dst_row_in_quad = (lane_id % 8) // 2
             quad_hori_id = lane_id % 2
             quad_vert_id = lane_id // 16
 
-            if row_id < 2 and lane_id % 16 < 8:
-                permute_factor = 0
-            elif row_id < 2:
-                permute_factor = 1
-            elif lane_id % 16 < 8:
-                permute_factor = 2
-            else:
-                permute_factor = 3
+            quad_hori_logical_id = lane_id % 16 // 8
+            quad_vert_logical_id = row_pair_id
+
+            permute_factor = quad_vert_logical_id * 2 + quad_hori_logical_id
 
             dst_col_in_quad = dst_row_in_quad ^ permute_factor
-
 
             dst_row_offset = quad_vert_id * 4 + dst_row_in_quad
             dst_col_offset = (quad_hori_id * 4 + dst_col_in_quad) * 8
 
             for i in range(8):
-                if dst_row_offset == 0 and dst_col_offset == 16:
-                    print(lane_id, permute_factor)
                 res[dst_row_offset, dst_col_offset + i] = lane_id
 
     return res
@@ -128,6 +120,30 @@ def shared_store_permuted_layout2():
             lane_id = 4 * (i % 8) + j // VEC_SIZE
             col_id = (lane_id % 8) ^ ((i // 2) % 4)
             res[i // 2, col_id * VEC_SIZE + j % VEC_SIZE] = lane_id
+
+    return res
+
+
+def shared_store_32x16_permuted_layout():
+    res = np.zeros((8, VEC_SIZE * 8), dtype="int32")
+    lane_ids = np.zeros((32, 16), dtype="int32")
+
+    for i in range(32):
+        for j in range(16):
+            lane_id = (i % 8) + 16 * ((i % 16) // 8) + (j // 8) * 8
+            lane_ids[i, j] = lane_id
+            row_pair_id = i // 16
+
+            row_id = row_pair_id * 2 + (lane_id // 16)
+            dst_row_in_quad = (lane_id % 8) // 2
+            dst_col_in_quad = dst_row_in_quad ^ row_id
+            quad_hori_id = lane_id % 2
+            quad_vert_id = (lane_id % 16) // 8
+
+            dst_row_offset =  quad_vert_id * 4 + dst_row_in_quad
+            dst_col_offset = (quad_hori_id * 4 + dst_col_in_quad) * VEC_SIZE
+
+            res[dst_row_offset, dst_col_offset + j % 8] = lane_id
 
     return res
 
