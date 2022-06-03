fd6555597efec1d91bdc0b3d74cfdfa71088f3e0
Masahiro Masuda <masahi129@gmail.com>
2022-06-03 19:02:52 +0900

    index for b trans case working

--- permute_test.py
+++ permute_test.py
@@ -75,6 +75,39 @@ np.testing.assert_equal(ref(), ldmatrix_permuted_layout())
 np.testing.assert_equal(ref(), ldmatrix_permuted_layout2())
 
 
+def ldmatrix_permuted_b_trans_layout():
+    res = np.ones((8, VEC_SIZE * 8), dtype="int32") * -1
+
+    for lane_id in range(32):
+        for row_pair_id in range(2):
+            row_id = row_pair_id * 2 + (lane_id // 16)
+            dst_row_in_quad = (lane_id % 8) // 2
+            quad_hori_id = lane_id % 2
+            quad_vert_id = lane_id // 16
+
+            if row_id < 2 and lane_id % 16 < 8:
+                permute_factor = 0
+            elif row_id < 2:
+                permute_factor = 1
+            elif lane_id % 16 < 8:
+                permute_factor = 2
+            else:
+                permute_factor = 3
+
+            dst_col_in_quad = dst_row_in_quad ^ permute_factor
+
+
+            dst_row_offset = quad_vert_id * 4 + dst_row_in_quad
+            dst_col_offset = (quad_hori_id * 4 + dst_col_in_quad) * 8
+
+            for i in range(8):
+                if dst_row_offset == 0 and dst_col_offset == 16:
+                    print(lane_id, permute_factor)
+                res[dst_row_offset, dst_col_offset + i] = lane_id
+
+    return res
+
+
 def shared_store_permuted_layout():
     res = np.zeros((8, VEC_SIZE * 8), dtype="int32")
 
