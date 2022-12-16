34bd82cfaa614f46597a87c76acc129f15ef47c0
Masahiro Masuda <masahi129@gmail.com>
2022-12-16 18:47:41 +0900

    tuning regnet

--- test_regnet.py
+++ test_regnet.py
@@ -8,8 +8,63 @@ import onnxruntime
 import onnx
 
 from tvm.contrib.download import download_testdata
+from tvm.tir.tensor_intrin.x86 import VNNI_DOT_16x4_INTRIN as VNNI_INTRIN
+from tvm import meta_schedule as ms
 from PIL import Image
 
+
+SCH_RULES_FOR_VNNI = [
+    ms.schedule_rule.ApplyCustomRule(),
+    ms.schedule_rule.AutoInline(
+        into_producer=False,
+        into_consumer=True,
+        inline_const_tensor=True,
+        disallow_if_then_else=True,
+        require_injective=True,
+        require_ordered=True,
+        disallow_op=["tir.exp"],
+    ),
+    ms.schedule_rule.AddRFactor(max_jobs_per_core=16, max_innermost_factor=64),
+    ms.schedule_rule.MultiLevelTilingWithIntrin(
+        VNNI_INTRIN,
+        structure="SSRSRS",
+        tile_binds=None,
+        max_innermost_factor=64,
+        vector_load_lens=None,
+        reuse_read=None,
+        reuse_write=ms.schedule_rule.ReuseType(
+            req="may",
+            levels=[1, 2],
+            scope="global",
+        ),
+    ),
+    ms.schedule_rule.MultiLevelTiling(
+        structure="SSRSRS",
+        tile_binds=None,
+        max_innermost_factor=64,
+        vector_load_lens=None,
+        reuse_read=None,
+        reuse_write=ms.schedule_rule.ReuseType(
+            req="may",
+            levels=[1, 2],
+            scope="global",
+        ),
+    ),
+    ms.schedule_rule.ParallelizeVectorizeUnroll(
+        max_jobs_per_core=16,
+        max_vectorize_extent=64,
+        unroll_max_steps=[0, 16, 64, 512],
+        unroll_explicit=True,
+    ),
+    ms.schedule_rule.RandomComputeLocation(),
+]
+
+POSTPROCS_FOR_VNNI = [
+    ms.postproc.DisallowDynamicLoop(),
+    ms.postproc.RewriteParallelVectorizeUnroll(),
+    ms.postproc.RewriteReductionBlock(),
+    ms.postproc.RewriteTensorize(vectorize_init_loop=True),
+]
 
 def get_input():
     img_url = "https://s3.amazonaws.com/model-server/inputs/kitten.jpg"
@@ -49,6 +104,39 @@ def run_tvm(mod, params, target):
     return module.get_output(0).numpy()[0]
 
 
+def tune(mod, params, target):
+    work_dir = "work"
+    with ms.Profiler() as profiler:
+        database = ms.relay_integration.tune_relay(
+            mod=mod,
+            target=target,
+            work_dir=work_dir,
+            max_trials_global=20000,
+            num_trials_per_iter=32,
+            max_trials_per_task=128,
+            params=params,
+            space=ms.space_generator.PostOrderApply(
+                sch_rules=SCH_RULES_FOR_VNNI,
+                postprocs=POSTPROCS_FOR_VNNI,
+            ),
+            module_equality="anchor-block",
+        )
+        lib = ms.relay_integration.compile_relay(
+            database=database,
+            mod=converted_mod,
+            target=target,
+            params=params,
+        )
+
+    print(profiler.table())
+
+    module = tvm.contrib.graph_executor.GraphModule(lib["default"](dev))
+    module.set_input("input", inp)
+    module.run()
+
+    return module.get_output(0).numpy().numpy()
+
+
 inp = get_input()
 
 onnx_path = "regnet_x_400mf_qint8.onnx"
@@ -58,8 +146,7 @@ ort_sess = onnxruntime.InferenceSession(onnx_path)
 onnx_input_dict = {"input": inp}
 ort_output = ort_sess.run(None, onnx_input_dict)[0][0]
 
-
-# model_onnx = onnx.load(onnx_path)
+model_onnx = onnx.load(onnx_path)
 # mod, params = relay.frontend.from_onnx(
 #     model_onnx,
 #     shape={"input": (1, 3, 224, 224)},
@@ -83,16 +170,20 @@ passes = tvm.transform.Sequential([
     relay.transform.FakeQuantizationToInteger(),
 ])
 
-target = "llvm"
+qmod = passes(mod)
 
-tvm_output = run_tvm(mod, params, target)
-tvm_output_quant = run_tvm(passes(mod), params, target)
+target = "llvm -mcpu=cascadelake -num-cores 6"
+
+# tvm_output = run_tvm(mod, params, target)
+# tvm_output_quant = run_tvm(qmod, params, target)
+tvm_output = tune(qmod, params, target)
+
 
 print(np.max(np.abs(tvm_output - ort_output)), np.mean(np.abs(tvm_output - ort_output)))
-print(np.max(np.abs(tvm_output - tvm_output_quant)), np.mean(np.abs(tvm_output - tvm_output_quant)))
+# print(np.max(np.abs(tvm_output - tvm_output_quant)), np.mean(np.abs(tvm_output - tvm_output_quant)))
 
 labels = get_labels()
 
-ranks = np.argsort(tvm_output_quant)[::-1]
+ranks = np.argsort(tvm_output)[::-1]
 for rank in ranks[0:5]:
     print("class='%s'" % labels[rank])
